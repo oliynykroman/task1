61c776dd4fab6c5658f0b63250db8f4e1db5974f
Roman oliynyk <oliynykroma@live.com>
2019-08-30 18:18:37 +0300

    token done

--- gitUser/src/app/services/user.service.ts
+++ gitUser/src/app/services/user.service.ts
@@ -4,7 +4,7 @@ import { User } from '../models/user';
 import { Repos } from '../models/repos';
 import { RequestOptionsArgs, Http, Headers } from '@angular/http';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-const TOKEN = "1c5a360c59b92c5849e13de1e016532e708b5e8c";
+const TOKEN = "198c6def89e39704bebad47edc3fb74f41c1f53f";
 const APIURL = 'https://api.github.com/users';
 // const APIURL = 'http://localhost:3000/';
 @Injectable({
@@ -18,26 +18,26 @@ export class UserService {
 
   private httpOptions = {
     headers: new HttpHeaders({
-      'Content-Type':  'application/json',
-
+      'Content-Type': 'application/json',
+      'Authorization': 'token ' + TOKEN
     })
   };
 
   constructor(private http: HttpClient) {
 
-   
+
   }
 
   createUrl(endPoint): string {
-    let url = this.apiServer + endPoint;   
+    let url = this.apiServer + endPoint;
     return url;
   }
 
 
-  public getUser(user: string, options?: RequestOptionsArgs){
-    return this.http.post<User>(this.createUrl('/users/?q=' + user), user, this.httpOptions);
+  public getUser(user: string, options?: RequestOptionsArgs) {
+    return this.http.get<User>(this.createUrl('/users/' + user), this.httpOptions);
   }
   public getUserRepos(user: string) {
-     return this.http.get<Repos>(`${this.apiServer}repos1`);
+    return this.http.get<Repos>(`${this.apiServer}repos1`);
   }
 }
