ec5b13f524f6bdda031abb26ffff93d171e6f09a
Roman Oliynyk <oliynykroma@live.com>
2019-09-01 07:48:10 +0300

    added fileds in model

--- gitUser/src/app/models/user.ts
+++ gitUser/src/app/models/user.ts
@@ -1,12 +1,12 @@
 export class User {
     public login?: string;
     public id?: string;
-    public node_id?:string;
+    public node_id?: string;
     public avatar_url?: string;
     public gravatar_url?: string;
-    public name?:string;
-    public location?:string;
-    public description:string;
+    public name?: string;
+    public location?: string;
+    public description: string;
     public url?: string;
     public html_url?: string;
     public followers_url?: string;
@@ -15,10 +15,13 @@ export class User {
     public starred_url?: string;
     public subscriptions_url?: string;
     public organizations_url?: string;
-    public repos_url? : string;
+    public repos_url?: string;
     public events_url?: string;
     public received_events_url?: string;
     public site_admin?: boolean;
     public score?: number;
     public public_repos?: number;
+    public company?: string;
+    public email?: string;
+    public length?: number;
 }
