b23c01a5eed6528b3a3fadee3d017ad83e34a4ee
Roman oliynyk <oliynykroma@live.com>
2019-09-02 12:44:06 +0300

    prepared for deploy

--- gitUser/src/app/services/user.service.ts
+++ gitUser/src/app/services/user.service.ts
@@ -7,7 +7,7 @@ import { SearchResults } from '../models/searchResults';
 import { throwError } from 'rxjs';
 import { catchError, retry } from 'rxjs/operators';
 
- const TOKEN = "9e6d85a2efc83b2b9782f99f6888af3556a6d7d4";
+// const TOKEN = "9e6d85a2efc83b2b9782f99f6888af3556a6d7d4";
 const APIURL = 'https://api.github.com/';
 
 // const APIURL = 'http://localhost:3000/';
@@ -23,20 +23,18 @@ export class UserService {
   private httpOptions = {
     headers: new HttpHeaders({
       'Content-Type': 'application/json',
-       'Authorization': 'token ' + TOKEN
+      //  'Authorization': 'token ' + TOKEN
     })
   };
 
   constructor(private http: HttpClient) {
-
-
+    //
   }
 
   createUrl(endPoint, data): string {
     let url = this.apiServer + '/' + endPoint + data;
     return url;
   }
-
 
   public searchUser(user: string, showOnPage: number = 10) {
     // &per_page=10&page=2
@@ -46,9 +44,11 @@ export class UserService {
   public getUserInfo(user: string) {
     return this.http.get<User>(this.createUrl('users/', user), this.httpOptions);
   }
+
   public getUserFollowers(followersoUrl: string) {
     return this.http.get<User>(followersoUrl, this.httpOptions);
   }
+  
   public getUserRepos(repoUrl: string) {
     return this.http.get<Repos[]>(repoUrl, this.httpOptions);
   }
